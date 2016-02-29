4529736d7d0c96807fede1d29d07886059c93e29
george@aprillemae.com <george@aprillemae.com>
2016-02-28 17:55:15 -0700

    Part 1 finished

--- Networkslab05/part1/LicenseManager.java
+++ Networkslab05/part1/LicenseManager.java
@@ -1,5 +1,7 @@
 package Networkslab05.part1;
 
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * Implementation of the Manager interface that
  * permits a number of licenses.
@@ -9,12 +11,19 @@ public class LicenseManager implements Manager
 {
     // the number of available permits
     private int permits;
+    // this is the lock
+    private ReentrantLock lck = new ReentrantLock();
   
     public LicenseManager(int permits) {
         if (permits < 0)
             throw new IllegalArgumentException();
-
+        // critical section
+        while(!lck.isHeldByCurrentThread()) {
+            lck.tryLock();
+        }
         this.permits = permits;
+        // end critical section
+        lck.unlock();
     }
   
     public boolean acquirePermit() {
