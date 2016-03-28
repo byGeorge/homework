a975bd76d551cb843c7e7af11558b8e5a830ff60
byGeorge <george@aprillemae.com>
2016-03-28 16:17:31 -0600

    Operating Systems homework 05 had to stop in class

---
+++ opsys_hw05/src/Priority.java
@@ -0,0 +1,44 @@
+/**
+ * FCFS scheduling algorithm.
+ */
+
+import java.util.List;
+
+public class Priority implements Algorithm
+{
+    private List<Task> queue;
+    private Task currentTask;
+
+    public Priority(List<Task> queue) {
+        this.queue = queue;
+    }
+
+    public void schedule() {
+        System.out.println("FCFS Scheduling \n");
+
+        while (!queue.isEmpty()) {
+            currentTask = pickNextTask();
+            
+            CPU.run(currentTask, currentTask.getBurst());
+
+            // we can get the current CPU time
+            CPU.getTime();
+        }
+    }
+
+    public Task pickNextTask() {
+        return null;
+    }
+
+    public double getAverageWaitTime() {
+        return 0;
+    }
+    
+    public double getAverageResponseTime() {
+        return 0;
+    }
+
+    public double getAverageTurnaroundTime() {
+        return 0;
+    }
+}
